e375298ae183e3879cd800e86889475134ade200
Arshit790 <arshitdixit5@gmail.com>
2024-11-11 22:01:47 +0530

    "adding header along with image and nav links"

--- App.js
+++ App.js
@@ -1,59 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// Nested Elements
-// For passing siblings => we have to pass the array of childrens in 3rd parameter
-// *const parent = React.createElement("div", { id: "parent" }, [
-//  * React.createElement("div", { id: "child" }, [
-//   *  React.createElement("h1", {}, "Hi I'm h1 Tag!"),
-//    * React.createElement("h2", {}, "Hi I'm h2 Tag!"),
-//   *]),
-//   *React.createElement("div", { id: "child2" }, [
-//    * React.createElement("h1", {}, "Hi I'm h1 Tag!"),
-//    * React.createElement("h2", {}, "Hi I'm h2 Tag!"),
-//   *]),
-// *]);
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+/**
+ * Header
+ *   Logo
+ *  Navbar
+ * Body
+ *   Search
+ *   Restaurant Container
+ *    -img
+ *    -name of res,stars,cuisine,delivery time
+ * Footer
+ *    -copyrights
+ *     -links
+ *    -address
+ *    -contact
+
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src="https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?semt=ais_hybrid" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// !root.render(parent);
-
-// ?React Element
-
-//? const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "React Renders work here 👌"
-// );
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-// JSX -> JSX is HTML like syntax
-
-// ! JSX (transpiled before it reaches to JS engine) = Parcel = Babel
-// ? JSX => Babel transpiles it to React.createElement =>   React ELement (JS OBject) => HTML Element(Render)
-
-const jsxHeading = <h1 id="heading">JSX Heading 😘</h1>;
-
-// root.render(jsxHeading);
-
-// console.log(heading, jsxHeading); // both are same without JSX or with JSX.
-
-// React Component
-
-const HeadingComponent2 = () => (
-  <div id="container">
-    {/* Injecting React Element using {} braces */}
-    {/* {title} */}
-    {1000 + 20000}
-    {console.log("Executing inside........")}
-    <h1>Helo React! </h1>
-  </div>
-);
-
-const title = (
-  <h1>
-    React with JSX🎈
-    <HeadingComponent2 />
-  </h1>
-);
-root.render(title );
+root.render(<AppLayout />);
