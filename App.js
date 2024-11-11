37fb2cfe1000e7ccef39ec95309f3173c855d0a3
Arshit790 <arshitdixit5@gmail.com>
2024-11-11 21:42:26 +0530

    "concepts of Core-React React Element JSX React Component Component--Composition "

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// Nested Elements
+// For passing siblings => we have to pass the array of childrens in 3rd parameter
+// *const parent = React.createElement("div", { id: "parent" }, [
+//  * React.createElement("div", { id: "child" }, [
+//   *  React.createElement("h1", {}, "Hi I'm h1 Tag!"),
+//    * React.createElement("h2", {}, "Hi I'm h2 Tag!"),
+//   *]),
+//   *React.createElement("div", { id: "child2" }, [
+//    * React.createElement("h1", {}, "Hi I'm h1 Tag!"),
+//    * React.createElement("h2", {}, "Hi I'm h2 Tag!"),
+//   *]),
+// *]);
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// !root.render(parent);
+
+// ?React Element
+
+//? const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "React Renders work here 👌"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// JSX -> JSX is HTML like syntax
+
+// ! JSX (transpiled before it reaches to JS engine) = Parcel = Babel
+// ? JSX => Babel transpiles it to React.createElement =>   React ELement (JS OBject) => HTML Element(Render)
+
+const jsxHeading = <h1 id="heading">JSX Heading 😘</h1>;
+
+// root.render(jsxHeading);
+
+// console.log(heading, jsxHeading); // both are same without JSX or with JSX.
+
+// React Component
+
+const HeadingComponent2 = () => (
+  <div id="container">
+    {/* Injecting React Element using {} braces */}
+    {/* {title} */}
+    {1000 + 20000}
+    {console.log("Executing inside........")}
+    <h1>Helo React! </h1>
+  </div>
+);
+
+const title = (
+  <h1>
+    React with JSX🎈
+    <HeadingComponent2 />
+  </h1>
+);
+root.render(title );
