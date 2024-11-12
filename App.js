6311a03c86be5dafccd086ba66b895748aae2800
Arshit790 <arshitdixit5@gmail.com>
2024-11-12 15:52:39 +0530

    "adding css to restro app and using components"

--- App.js
+++ App.js
@@ -39,14 +39,44 @@ const Header = () => {
   );
 };
 
+// const styleCard = {
+//   backgroundColor: "#f0f0f0",
+// };
+
+const RestaurantCard = ({ name, cuisines, ratings, ETA }) => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines}</h4>
+      <h4>{ratings}⭐</h4>
+      <h4>{ETA}mins</h4>
+    </div>
+  );
+};
+
 const Body = () => {
   return (
     <div className="body">
-      <div className="search-box">
-        <input type="text" />
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard
+          name="Meghana Foods"
+          cuisines="Biryani, North-Indian"
+          ratings="4.4"
+          ETA="38"
+        />
+        <RestaurantCard
+          name="KFC"
+          cuisines="Fast Foods"
+          ratings="4.1"
+          ETA="20"
+        />
       </div>
-
-      <div className="restaurant-container"></div>
     </div>
   );
 };
