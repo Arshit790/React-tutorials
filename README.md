572bb0785aa54a5f9318d21eb2317f1f079e8820
Arshit790 <arshitdixit5@gmail.com>
2024-11-12 22:10:08 +0530

    "adding notes"

--- README.md
+++ README.md
@@ -96,3 +96,11 @@ root.render(title);
 - -contact
 
 \*/
+
+Two types of Export/Import
+
+Default Export/Import
+export default Component; import Component from "path";
+
+Named Export/Import
+export const Component; import {Component} from "path";
--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
 
-// const styleCard = {
-//   backgroundColor: "#f0f0f0",
-// };
-
-
 
 const AppLayout = () => {
   return (
