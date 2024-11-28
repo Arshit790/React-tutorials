058268c96974de26f740c0650bf3db99632282e9
Arshit790 <arshitdixit5@gmail.com>
2024-11-28 18:11:50 +0530

    "intro to custom hooks"

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,21 +2,13 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { CDN_URL } from "../utils/constants";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
 import { IoIosArrowDropdown } from "react-icons/io";
+import useRestauranMenu from "../utils/useRestaurantMenu";
+
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
   const [toggle, setToggle] = useState({});
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestauranMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
