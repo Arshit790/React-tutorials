fadb9a84ea217d35cac46aef3d71c665ade87298
Arshit790 <arshitdixit5@gmail.com>
2024-12-29 20:10:55 +0530

    "adding accordian"

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -3,7 +3,9 @@ const RestaurantCategory = ({ data }) => {
     <div className="w-6/12">
       {/* Header */}
       <div className="flex justify-between text-center bg-gray-50 shadow-lg p-4 my-4 mx-auto">
-        <span className="font-bold">{data.title}</span>
+        <span className="font-bold">
+          {data.title}({data.itemCards.length})
+        </span>
         <span>⬇️</span>
       </div>
       {/* Accordian Body */}
